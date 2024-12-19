7f05808b7742f24afaff581dd453888a040bd3a4
Omar <Omar@DESKTOP-V34HHN9>
2024-12-20 01:17:18 +0200

    Add project files.

---
+++ CSharp01R2S2/Program.cs
@@ -0,0 +1,114 @@
+﻿#region Task1
+//Console.WriteLine("My name is \"Omar Mohamed Ahmed Mady\" ");
+//Console.WriteLine("I have 22 year");
+//Console.WriteLine("My favorite hobby is playing playstation");
+#endregion
+
+#region Task2
+////Declaretion
+//String MyName;
+//int DOB;
+//double pie;
+//bool IsAlive;
+
+////Assigned
+//MyName = "Omar";
+//DOB = 2002;
+//pie = 3.14;
+//IsAlive = false ;
+#endregion
+
+#region Task3
+//Console.Write("Pleas enter value of Celsius : ");
+//double DegreeOfCelsius = int.Parse(Console.ReadLine());
+//double DegreeOfFehrenhite = (DegreeOfCelsius * 1.8) + 32;
+//Console.WriteLine($"The degree of Fehrenhit : {DegreeOfFehrenhite}");
+#endregion
+
+#region Task4
+//Console.Write("Pleas enter your first number : ");
+//String FirstNumber = Console.ReadLine();
+//int IntFirstNumber = int.Parse( FirstNumber);
+//Console.Write("Pleas enter your second number : ");
+//String SecondNumber = Console.ReadLine();
+//int IntSecondNumber = int.Parse(SecondNumber);
+////Operation
+//int Addition = IntFirstNumber + IntSecondNumber;
+//int Subtraction = IntFirstNumber - IntSecondNumber;
+//int Multiplication = IntFirstNumber * IntSecondNumber;
+//double Division = IntFirstNumber / IntSecondNumber;
+//Console.WriteLine("Addition : " + IntFirstNumber + " + " + IntSecondNumber + " = " + Addition);
+//Console.WriteLine("Subtraction = " + IntFirstNumber + " - " + IntSecondNumber + " = " + Subtraction);
+//Console.WriteLine("Multiplication = " + IntFirstNumber + " * " + IntSecondNumber + " = " + Multiplication);
+//Console.WriteLine("Division = " + IntFirstNumber + " / " + IntSecondNumber + " = " + Division);
+#endregion
+
+#region Task5
+
+//Console.Write("Pleas enter length : ");
+//String Length = Console.ReadLine();
+//int IntLength = int.Parse(Length);
+//Console.Write("Pleas enter width : ");
+//String Width = Console.ReadLine();
+//int IntWidth = int.Parse(Width);
+//double Area = IntLength * IntWidth;
+//Console.WriteLine("Display of area = "+ Area);
+
+
+#endregion
+
+#region Task6
+//Console.Write("Pleas enter Your Name : ");
+//String Number = Console.ReadLine();
+//int IntNumber = int.Parse(Number);
+//if(IntNumber % 2 == 0)
+//{
+//    Console.WriteLine("Your number is \"even\"");
+//}
+//else
+//{
+//    Console.WriteLine("Your number is \"odd\"");
+
+//}
+#endregion
+
+#region Task7
+//Console.Write("Pleas enter Your age : ");
+//String Age = Console.ReadLine();
+//int IntAge = int.Parse(Age);
+//if (IntAge >= 18)
+//{
+//    Console.WriteLine("You are eligible to vote.");
+//}
+//else
+//{
+//    Console.WriteLine("You are not eligible to vote.");
+
+//}
+#endregion
+
+#region Task8
+//Console.Write("Pleas enter Your score  : ");
+//int Score = int.Parse(Console.ReadLine());
+//if (100 < Score || 0 > Score)
+//{
+//    Console.WriteLine("Your grade is Not correct");
+//}
+//else if (Score >= 90)
+//{
+//    Console.WriteLine("Your grade is : A ");
+//}
+//else if (Score >= 80)
+//{
+//    Console.WriteLine("Your grade is : B ");
+//} else if (Score >= 70)
+//{
+//    Console.WriteLine("Your grade is : C ");
+//} else if (Score >= 60)
+//{
+//    Console.WriteLine("Your grade is : D ");
+//}
+//else Console.WriteLine("Your grade is : f ");
+
+#endregion
+
